4e929cb2d6ee475d4c4617e8c165ea4887bd2bfa
Jayavel <jayavel1520@gmail.com>
2025-05-13 23:31:44 +0530

    Refactor server code to simplify response handling and remove unused routes

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//     if (req.url === "/") {
+//         res.writeHead(200, "Successfully executed");
+//         res.end("Hi, welcome to the Node.js Server Home page");
+//     } else if (req.url === "/about") {
+//         res.writeHead(200,"Successfully executed");
+//         res.end("This is the about page!");
+//     } else {
+//         res.writeHead(404, "Path does not exist");
+//         res.end("404 Not Found");
+//     }
+// });
+
+// server.listen(3000, () => {
+//     console.log("Server is successfully running at: http://localhost:3000");
+// });
+
+
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.writeHead(200, "Successfully executed");
-        res.end("Hi, welcome to the Node.js Server Home page");
-    } else if (req.url === "/about") {
-        res.writeHead(200,"Successfully executed");
-        res.end("This is the about page!");
-    } else {
-        res.writeHead(404, "Path does not exist");
-        res.end("404 Not Found");
-    }
-});
+const server = http.createServer((req, res)=>{
+    res.setHeader("Content-type", "text/html");
+    res.write(`<script src='/script.js'></script>`);
+    res.end();
+})
 
-server.listen(3000, () => {
-    console.log("Server is successfully running at: http://localhost:3000");
-});
+server.listen(3000);
